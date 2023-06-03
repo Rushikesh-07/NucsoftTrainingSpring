a562a5e90726de07c05c1f7503f95af513284027
Rushikesh <rushikeshbaraskar1@gmail.com>
2023-06-03 18:43:44 +0530

    my name is added in sim calling method

--- src/main/java/mobile/company/mobile/Airtel.java
+++ src/main/java/mobile/company/mobile/Airtel.java
@@ -3,6 +3,6 @@ package mobile.company.mobile;
 public class Airtel implements SIM {
     
     public void phoneCall() {
-        System.out.println("Making a phone call using Airtel SIM.");
+        System.out.println("Making a phone call from Rushikesh's phone using Airtel SIM.");
     }
 }
--- src/main/java/mobile/company/mobile/JIO.java
+++ src/main/java/mobile/company/mobile/JIO.java
@@ -3,6 +3,6 @@ package mobile.company.mobile;
 public class JIO implements SIM {
     @Override
     public void phoneCall() {
-        System.out.println("Making a phone call using JIO SIM.");
+        System.out.println("Making a phone call Rushikesh's phone using JIO SIM.");
     }
 }

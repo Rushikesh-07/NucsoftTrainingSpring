fb430ff96bcc3f6bbce1080c09fd0be7f5fa2798
Rushikesh <rushikeshbaraskar1@gmail.com>
2023-06-04 16:16:40 +0530

    alternate mobile number is added by colleague

--- src/main/java/mobile/company/mobile/Airtel.java
+++ src/main/java/mobile/company/mobile/Airtel.java
@@ -4,5 +4,7 @@ public class Airtel implements SIM {
     
     public void phoneCall() {
         System.out.println("Making a phone call from Rushikesh's phone using Airtel SIM.");
+        System.out.println("Phone Number is------> 9969409040");
+        System.out.println("Alternate Phone Number is------> 9869102525");
     }
 }
